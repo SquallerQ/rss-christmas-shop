cef6cb84450cf6f58bf6a73a88609635c5ea9702
zSqualler <john199502031@gmail.com>
2024-11-27 22:59:51 +0100

    feat: add burger menu on main page

--- christmas-shop/pages/main/script.js
+++ christmas-shop/pages/main/script.js
@@ -1,1 +1,33 @@
-//Start
+// Burger
+const menuHeader = document.querySelector(".header__menu-list");
+const burgerButton = document.querySelector(".burger-button");
+const bodyForScroll = document.querySelector("body");
+const menuItemsList = document.querySelectorAll(".header__menu-item");
+
+//Click on burger button
+burgerButton.addEventListener("click", function () {
+  // If menuHeader havent class active, add this class. Open menu
+  if (!menuHeader.classList.contains("header__menu-list--active")) {
+    // Open menu
+    menuHeader.classList.add("header__menu-list--active");
+    // Turn burger button
+    burgerButton.classList.add("burger-button--active");
+    // Disable scrolling
+    bodyForScroll.style.overflow = "hidden";
+  } else {
+    // Close menu
+    menuHeader.classList.remove("header__menu-list--active");
+    // Turn off burger button
+    burgerButton.classList.remove("burger-button--active");
+    // Enable scroll
+    bodyForScrollAndShadow.style.overflow = "";
+  }
+});
+menuItemsList.forEach(function(item) {
+  item.addEventListener('click', function() {
+    // Close menu
+    menuHeader.classList.remove("header__menu-list--active");
+    // Turn off burger button
+    burgerButton.classList.remove("burger-button--active");
+  })
+}) 
