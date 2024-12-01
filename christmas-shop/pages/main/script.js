2ee2502aff98f1372bfeb59df5c3b75d7c037742
zSqualler <john199502031@gmail.com>
2024-12-01 17:22:55 +0100

    feat: finish slider

--- christmas-shop/pages/main/script.js
+++ christmas-shop/pages/main/script.js
@@ -35,91 +35,97 @@ menuItemsList.forEach(function (item) {
 });
 
 // Slider
-// const oneCard = document.querySelector(".slider__card");
-// console.log(cardsContainer.offsetWidth);
-// const pageContainer = document.querySelector(".container")
 const cardsContainer = document.querySelector(".slider__cards");
 const leftButton = document.querySelector(".slider__buttons-left");
 const rightButton = document.querySelector(".slider__buttons-right");
 
 let scrollRelativeToStart = 0;
 let scrollOnClick = 0;
+let clickCounter = 0;
+let clickCounterMax = 0;
 
 function calculateScrollProgress(symbol) {
   const screenWidth = window.innerWidth;
   const containerWidth = 1993;
-
+  let clickNeedToScrollEnd;
   let pageContainer;
 
   if (screenWidth > 1400) {
+    clickNeedToScrollEnd = 3
     pageContainer = 41;
-    scrollOnClick = (containerWidth - 1440) / 3 + pageContainer;
+    scrollOnClick = (containerWidth - 1440) / clickNeedToScrollEnd + pageContainer;
   } else if (screenWidth > 1200) {
+    clickNeedToScrollEnd = 3
     pageContainer = 41;
-    scrollOnClick = (1993 - screenWidth) / 3 + pageContainer;
+    scrollOnClick = (1993 - screenWidth) / clickNeedToScrollEnd + pageContainer;
   } else if (screenWidth > 768) {
+    clickNeedToScrollEnd = 3
     pageContainer = 8;
-    scrollOnClick = (1993 - screenWidth) / 3 + pageContainer;
+    scrollOnClick = (1993 - screenWidth) / clickNeedToScrollEnd + pageContainer;
   } else {
+    clickNeedToScrollEnd = 6
     pageContainer = 4;
-    scrollOnClick = (1993 - screenWidth) / 6 + pageContainer;
+    scrollOnClick = (1993 - screenWidth) / clickNeedToScrollEnd + pageContainer;
   }
 
   oneStepScrolling = Math.trunc(scrollOnClick);
   if (symbol === '+') {
-    scrollRelativeToStart = scrollRelativeToStart - oneStepScrolling;
-    console.log('minus');
-    
+    scrollRelativeToStart = scrollRelativeToStart - oneStepScrolling;    
   } else {
     scrollRelativeToStart = scrollRelativeToStart + oneStepScrolling;
-    console.log('plus');
-
   }
 
-  console.log(screenWidth);
-  
-  console.log(oneStepScrolling);
-  console.log(scrollRelativeToStart);
   cardsContainer.style.marginLeft = `${-scrollRelativeToStart}px`;
+}
+function buttonsUpdate(maxClick) {
+
+  console.log(maxClick);
+  // for Left button 
+  if (clickCounter === 0) {
+    leftButton.classList.add("slider__button--disabled");
+  } else {
+    leftButton.classList.remove("slider__button--disabled");
+  }
+  // for Right button 
+  if (clickCounter === maxClick) {
+    rightButton.classList.add("slider__button--disabled");
+  } else {
+    rightButton.classList.remove("slider__button--disabled");
+  }
 }
 
 rightButton.addEventListener("click", function () {
   const _symbol = "-";
-  calculateScrollProgress(_symbol);
+  const screenWidth = window.innerWidth;
+  if (screenWidth > 768) {
+    clickCounterMax = 3;
+  } else {
+    clickCounterMax = 6;
+  }
+  
+  if (clickCounterMax > clickCounter) {
+    clickCounter = clickCounter + 1;
+    calculateScrollProgress(_symbol);
+    buttonsUpdate(clickCounterMax);
+  }
 });
-
 
 leftButton.addEventListener("click", function () {
   const _symbol = '+'
-  calculateScrollProgress(_symbol);
+  if (clickCounter > 0) {
+    clickCounter = clickCounter - 1;
+    calculateScrollProgress(_symbol);
+    buttonsUpdate()
+  }
 });
 
+window.addEventListener("resize", function () {
+  scrollRelativeToStart = 0;
+  clickCounter = 0;
+  cardsContainer.style.marginLeft = "0px";
+  
+  const screenWidth = window.innerWidth;
+  clickCounterMax = screenWidth > 768 ? 3 : 6;
 
-
-
-
-// leftButton.addEventListener("click", function () {
-//   const screenWidth = window.screen.width;
-//   // let clickCounter;
-//   let pageContainer;
-//   let scrollOnClick;
-//   if (screenWidth > 1400) {
-//     pageContainer = 41;
-//     scrollOnClick = (1993 - 1440) / 3 + pageContainer;
-//   } else if (screenWidth > 1200) {
-//     pageContainer = 41;
-//     scrollOnClick = (1993 - screenWidth) / 3 + pageContainer;
-//   } else if (screenWidth > 768) {
-//     pageContainer = 8;
-//     scrollOnClick = (1993 - screenWidth) / 3 + pageContainer;
-//   } else {
-//     pageContainer = 4;
-//     scrollOnClick = (1993 - screenWidth) / 6 + pageContainer;
-//   }
-//   console.log(screenWidth);
-
-//   oneStepScrolling = scrollOnClick;
-
-//   scrollRelativeToStart = scrollRelativeToStart + oneStepScrolling;
-//   cardsContainer.style.marginLeft = `${scrollRelativeToStart}px`;
-// });
+  buttonsUpdate(clickCounterMax);
+});
